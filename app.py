104fc14ba941795a695b8d09400042e201b5b430
kuroneko2828 <nekonekonya.kuro@gmail.com>
2022-04-29 23:12:56 +0900

    for_deploy

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = "Hello, World"
-    return name
+    name = "名無し"
+    return render_template('index.html', title='flask test', name=name)
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
+@app.route('/<name>')
+def good(name):
+    return render_template('index.html', title='flask test', name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
