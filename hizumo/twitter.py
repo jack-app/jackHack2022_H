6f69d4a7b0a98d70666f17a9fbe5b34cb5aa6044
hizumo <hajime0630@mbr.nifty.com>
2022-04-30 15:05:36 +0900

    twitter

---
+++ hizumo/twitter.py
@@ -0,0 +1,13 @@
+thm="お題"
+sen="五感"
+ans="自分の回答"
+per="一致度"
+web="https://fivesenseh.herokuapp.com/"
+
+#１のところに結果を入れる
+print(thm+"1")
+print(sen+"1")
+print(ans+"1")
+print(per+"1")
+print(web)
+print("#特徴一致ゲーム")
